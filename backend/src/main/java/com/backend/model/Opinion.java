08b599180c066890f1d872aeac22aaedd350bda9
247686 Kaczmarek Jakub <247686@edu.p.lodz.pl>
2025-04-07 17:56:13 +0200

    Opinion and User classes implemented

--- backend/src/main/java/com/backend/model/Opinion.java
+++ backend/src/main/java/com/backend/model/Opinion.java
@@ -2,18 +2,77 @@ package com.backend.model;
 
 import jakarta.persistence.*;
 
+import java.util.UUID;
+
 @Entity
 @Table(name = "opinions")
 public class Opinion {
+
+
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    private Long id;
+    private UUID id;
 
     @ManyToOne
     @JoinColumn(name = "shop_id")
     private Shop shop;
 
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
     private String description;
 
     private int stars;
+
+    public Opinion(UUID id, Shop shop, User user, String description, int stars) {
+        this.id = id;
+        this.shop = shop;
+        this.user = user;
+        this.description = description;
+        this.stars = stars;
+    }
+
+    public Opinion() {
+
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public Shop getShop() {
+        return shop;
+    }
+
+    public void setShop(Shop shop) {
+        this.shop = shop;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getStars() {
+        return stars;
+    }
+
+    public void setStars(int stars) {
+        this.stars = stars;
+    }
 }
--- backend/src/main/java/com/backend/model/User.java
+++ backend/src/main/java/com/backend/model/User.java
@@ -1,16 +1,18 @@
 package com.backend.model;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.OneToMany;
-import org.springframework.data.annotation.Id;
+import jakarta.persistence.*;
 
 import java.util.List;
+import java.util.UUID;
 
+@Entity
+@Table(name = "users")
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    private Long id;
+    private UUID id;
+
     private String firstName;
     private String lastName;
     private String email;
@@ -19,7 +21,106 @@ public class User {
     private int tier;
     private String img;
 
-    @OneToMany(mappedBy = "user")
+    @OneToMany
     private List<Shop> shops;
 
+    public User() {
+    }
+
+    public User(String firstName, String lastName, String email, String password, String phoneNumber, int tier, String img) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.tier = tier;
+        this.img = img;
+    }
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getTier() {
+        return tier;
+    }
+
+    public void setTier(int tier) {
+        this.tier = tier;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public List<Shop> getShops() {
+        return shops;
+    }
+
+    public void setShops(List<Shop> shops) {
+        this.shops = shops;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", tier=" + tier +
+                ", img='" + img + '\'' +
+                '}';
+    }
+
+
 }
+
