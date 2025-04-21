45dbcb3809d1ec71aa91c3ba1f07ef9d8a06e53b
247634 Borowski Damian <247634@edu.p.lodz.pl>
2025-04-21 22:53:20 +0200

    Small adjustments to map component

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -3,9 +3,11 @@ import Navbar from '../components/Navbar';
 
 export default function MapView() {
   return (
-    <main className="w-full h-screen">
-        <Navbar />
-      <MapClientWrapper />
+    <main className="w-full h-screen overflow-hidden flex flex-col">
+      <Navbar />
+      <div className="flex-1">
+        <MapClientWrapper />
+      </div>
     </main>
   );
 }
