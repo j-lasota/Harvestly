24d0c44747b09345a94d13a9dafee2b4a0b7595b
miloszwierucki <milosz@wierucki.com>
2025-04-25 15:07:25 +0200

    Add type generate scripts to GraphQL

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,28 @@
 // import Image from "next/image";
 
-export default function Home() {
+import { query } from "@/graphql/apollo-client";
+import { graphql } from "gql.tada";
+
+const CountriesQuery = graphql(`
+  query Countries {
+    countries {
+      name
+      capital
+      code
+      continent {
+        code
+        name
+      }
+      currency
+    }
+  }
+`);
+
+export default async function Home() {
+  const { data } = await query({ query: CountriesQuery });
+
+  console.log(data);
+
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20"></div>
   );
