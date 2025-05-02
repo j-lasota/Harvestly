fe5e7f1d1e776f85526727d449ee5efa9ed3d643
247731 Marszałek Piotr <247731@edu.p.lodz.pl>
2025-05-02 15:20:09 +0200

    Add user management functionality with GraphQL API

---
+++ backend/src/main/java/com/backend/controller/UserController.java
@@ -0,0 +1,48 @@
+package com.backend.controller;
+
+import com.backend.model.User;
+import com.backend.service.UserService;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @QueryMapping
+    public Optional<User> userById(@Argument Long id) {
+        return userService.getUserById(id);
+    }
+
+    @QueryMapping
+    public List<User> users() {
+        return userService.getAllUsers();
+    }
+
+    @MutationMapping
+    public User createUser(@Argument String firstName, @Argument String lastName, @Argument String email,
+                           @Argument String password, @Argument String phoneNumber, @Argument String img) {
+        return userService.saveUser(new User(firstName, lastName, email, password, phoneNumber, 1,img));
+    }
+
+    @MutationMapping
+    public User updateUser(@Argument Long id, @Argument String firstName, @Argument String lastName,
+                           @Argument String email, @Argument String password, @Argument String phoneNumber,
+                           @Argument Integer tier, @Argument String img) {
+        return userService.updateUser(id, firstName, lastName, email, password, phoneNumber, tier, img);
+    }
+
+    @MutationMapping
+    public Boolean deleteUser(@Argument Long id) {
+        return userService.deleteUserById(id);
+    }
+}
