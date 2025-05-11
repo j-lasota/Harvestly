442fbcc4098b0c3aa2e757c99ef2ec44975c2dee
Ayyker <247717@edu.p.lodz.pl>
2025-05-11 18:14:34 +0200

    Auth0 SYNCHRONIZATION WORKS

--- backend/src/main/java/com/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/backend/config/SecurityConfig.java
@@ -1,30 +1,46 @@
 package com.backend.config;
 
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Value("${OAUTH2_ISSUER}")
+    private String issuer;
+
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .cors(cors -> {})
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/").permitAll()
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .authorizeHttpRequests(auth -> auth
+                        //.requestMatchers("/graphql").authenticated()
+                        //.requestMatchers("/*", "/graphiql/**").permitAll()
+                        //.anyRequest().authenticated()
+                        .anyRequest().permitAll()
                 )
-                .oauth2Login(withDefaults());
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt.decoder(jwtDecoder()))
+                );
 
         return http.build();
     }
 
+    @Bean
+    JwtDecoder jwtDecoder() {
+        return JwtDecoders.fromOidcIssuerLocation(issuer);
+    }
 }
