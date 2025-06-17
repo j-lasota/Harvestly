d0f540a1c9f0ea0f9874b93d60b9aece583d451d
miloszwierucki <milosz@wierucki.com>
2025-06-17 03:51:00 +0200

    Simple edit page

--- app/[locale]/store/[storeSlug]/edit/page.tsx
+++ app/[locale]/store/[storeSlug]/edit/page.tsx
@@ -1,1 +1,27 @@
-sfs;
+import { ContainerWrapper } from "@/components/layout/container-wrapper";
+
+import { auth } from "@/auth";
+
+export default async function EditStorePage({
+  params,
+}: Readonly<{
+  params: Promise<{
+    storeSlug: string;
+  }>;
+}>) {
+  const session = await auth();
+  const userId = session?.user?.id;
+
+  const { storeSlug } = await params;
+
+  console.log(storeSlug, userId);
+
+  return (
+    <ContainerWrapper
+      comp="main"
+      className="mt-10 mb-16 flex min-h-screen flex-col gap-8 md:mt-10"
+    >
+      <div>Edit</div>
+    </ContainerWrapper>
+  );
+}
